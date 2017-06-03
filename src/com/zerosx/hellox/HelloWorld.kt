1979c2a9bd66dc345e7d7cf6f3b0737aeb4375e1
rithikied <rithikied@hotmail.com>
2017-06-03 04:38:03 -0700

    Add Check type on input + - * /

--- src/com/zerosx/hellox/HelloWorld.kt
+++ src/com/zerosx/hellox/HelloWorld.kt
@@ -6,8 +6,17 @@ fun main(args: Array<String>) {
     println("Please Input 1st number Examle 5.0")
     var a: String = readLine().toString()
     println("Please Input + - * / ")
-    var type = readLine()
-    if(type!="+"&&type!="-"&&type!="*"&&type!="/")
+    var checkx = 1
+    var i = 1
+    var type = ""
+    do {
+        type = readLine().toString()
+        if (type != "+" && type != "-" && type != "*" && type != "/") {
+            println("Error please write + - * /")
+        } else {
+            i--
+        }
+    } while (i > 0)
     println("Please Input 2nd number Examle 5.0")
     var b: String = readLine().toString()
     if (type == "+") {
@@ -20,18 +29,22 @@ fun main(args: Array<String>) {
         divied(a.toDouble(), b.toDouble())
     }
 }
+
 fun crossx(a: Double, b: Double) {
     var resu = a * b;
     println(resu)
 }
+
 fun passx(a: Double, b: Double) {
     var resu = a + b;
     println(resu)
 }
+
 fun minusx(a: Double, b: Double) {
     var resu = a - b;
     println(resu)
 }
+
 fun divied(a: Double, b: Double) {
     var resu = a / b;
     println(resu)
