bd3b29ec1de84eb3810a25d0d0f45c89f8e5924f
rithikied <rithikied@hotmail.com>
2017-06-03 04:26:51 -0700

    initial commit

---
+++ src/com/zerosx/hellox/HelloWorld.kt
@@ -0,0 +1,38 @@
+package com.zerosx.hellox
+
+import kotlin.concurrent.timerTask
+
+fun main(args: Array<String>) {
+    println("Please Input 1st number Examle 5.0")
+    var a: String = readLine().toString()
+    println("Please Input + - * / ")
+    var type = readLine()
+    if(type!="+"&&type!="-"&&type!="*"&&type!="/")
+    println("Please Input 2nd number Examle 5.0")
+    var b: String = readLine().toString()
+    if (type == "+") {
+        passx(a.toDouble(), b.toDouble())
+    } else if (type == "-") {
+        minusx(a.toDouble(), b.toDouble())
+    } else if (type == "*") {
+        crossx(a.toDouble(), b.toDouble())
+    } else if (type == "/") {
+        divied(a.toDouble(), b.toDouble())
+    }
+}
+fun crossx(a: Double, b: Double) {
+    var resu = a * b;
+    println(resu)
+}
+fun passx(a: Double, b: Double) {
+    var resu = a + b;
+    println(resu)
+}
+fun minusx(a: Double, b: Double) {
+    var resu = a - b;
+    println(resu)
+}
+fun divied(a: Double, b: Double) {
+    var resu = a / b;
+    println(resu)
+}
